d5a86530b619384673487062813068a47442d45b
advtek02.tn <advtek02.tn@innolux.com>
2025-10-09 14:18:38 +0800

    調整 popup message width

--- src/components/PopupMessage.vue
+++ src/components/PopupMessage.vue
@@ -23,7 +23,7 @@
 
 <template>
 
-    <div id="alertMsg" class="toast w-8/10">
+    <div id="alertMsg" class="toast w-10/10">
         <div class="alert w-10/10 text-white text-xl" :class="{ 'alert-success': props.status == true, 'alert-error': props.status == false }">
             <span>{{ moment().format("HH:MM:SS") }} </span><span>{{ props.message }}</span>
         </div>
