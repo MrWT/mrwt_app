0c49a25e8952161103bcef8ba3ede89ba5feff97
advtek02.tn <advtek02.tn@innolux.com>
2025-10-13 10:54:09 +0800

    調整 layout

--- src/components_kuoFund/SettingActivity.vue
+++ src/components_kuoFund/SettingActivity.vue
@@ -15,19 +15,20 @@
         init();
     });
 
+    let uploadedFileName = ref("");
     let manual = reactive({
         location_name: "",
         page: "",
         memo: "",
-    });
+    });    
     let locations = reactive([]);
 
     // 初始化 component
     function init(){
-        console.log("Setting_Activity.init");
-        console.log("props.app_state", props.app_state);
-        console.log("props.title", props.title);
-        console.log("props.account", props.account);
+        //console.log("Setting_Activity.init");
+        //console.log("props.app_state", props.app_state);
+        //console.log("props.title", props.title);
+        //console.log("props.account", props.account);
 
         fetchActivity();
     }
@@ -45,10 +46,15 @@
             });
         });
     }
+    // 觸發上傳檔案
+    function triggerUploadFile(){
+        document.getElementById("fileInput").click();
+    }
     // 轉換圖檔成 base64
     function change_image_to_base64(event){
-        console.log("change_image_to_base64");
         const file = event.target.files[0];
+        uploadedFileName.value = file.name;
+        console.log("change_image_to_base64.file=", uploadedFileName.value);
 
         if (file) {
             const reader = new FileReader();
@@ -111,8 +117,8 @@
 <template>
 
     <div class="divider">新增活動手冊頁面</div>
-    <div class="w-10/10 flex flex-row gap-2 place-items-center">
-        <div class="w-5/10 flex flex-col">
+    <div class="w-10/10 flex flex-col md:flex-row gap-2 place-items-center">
+        <div class="w-10/10 md:w-5/10 flex flex-col">
             <label class="label">
                 地點:
                 <input type="text" class="input w-10/10" placeholder="" v-model="manual.location_name" list="locations" />
@@ -122,21 +128,23 @@
             </label>
         </div>
 
-        <div class="w-5/10 flex flex-col">
+        <div class="w-10/10 md:w-5/10 flex flex-col">
             <label class="label">
                 圖檔:
-                <input type="file" id="fileInput" @change="change_image_to_base64" />
+                <input type="file" class="hidden" id="fileInput" accept="image/png, image/jpeg" @change="change_image_to_base64" />
+                <button class="btn btn-primary" title="只接受 .png, .jpg" @click="triggerUploadFile">上傳檔案</button>
+                <span>{{ uploadedFileName }}</span>
             </label>
         </div>
     </div>
-    <div class="w-10/10 flex flex-row gap-2 place-items-center">
-        <div class="w-5/10 flex flex-col">
+    <div class="w-10/10 flex flex-col md:flex-row gap-2 mt-2 place-items-center">
+        <div class="w-10/10 md:w-5/10 flex flex-col">
             <label class="label">
                 備註:
                 <input type="text" class="input w-10/10" placeholder="" v-model="manual.memo" />
             </label>
         </div>
-         <button class="btn btn-neutral w-5/10" @click="new_manual_page">
+         <button class="btn btn-neutral w-10/10 md:w-5/10" @click="new_manual_page">
             NEW
         </button>
     </div>
