13e0998d390909ca51f11e0ece73a2c39a319c26
advtek02.tn <advtek02.tn@innolux.com>
2025-10-20 13:26:07 +0800

    1. 調整 ai_role 資料結構
    2. 調整 settingModal layout

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -67,12 +67,12 @@
             let speaker = "";
             let short_name = "";
             aiRoles.forEach((roleObj, role_i) => {
-                if(roleObj["role"] === values[0]["ai_role"]){
+                if(roleObj["code"] === values[0]["ai_role"]){
                     speaker = roleObj["name"];
-                    short_name = roleObj["name"].substr(0, 1);
+                    short_name = roleObj["short_name"];
 
-                    currentAiRole["nation"] = roleObj["role"].split("_")[0];
-                    currentAiRole["gender"] = roleObj["role"].split("_")[1];
+                    currentAiRole["nation"] = roleObj["nation"];
+                    currentAiRole["gender"] = roleObj["gender"];
                 }
             });
 
@@ -93,6 +93,9 @@
     }
     // 送出 message
     function send(){
+        // 當沒有 keyin message 時, 不送出訊息
+        if(!userMessage.value) return;
+
         let user_name = userInfo.language === "EN" ? userInfo.name : userInfo.cname;
 
         messages.push({
@@ -122,32 +125,33 @@
     // 改變 AI 角色
     function changeAiRole(){
         console.log("changeAiRole.currentAiRole=", currentAiRole);
-        let msg = "換成";
+        let msg = "talk_with_";
         switch(currentAiRole.nation){
-            case "kr":
-                msg += "韓國";
+            case "Korea":
+                msg += "kr_";
                 break;
-            case "jp":
-                msg += "日本";
+            case "Japan":
+                msg += "jp_";
                 break;
-            case "tw":
-                msg += "台灣";
-                break;
-            case "us":
-                msg += "美國";
+            case "Taiwan":
+                msg += "tw_";
                 break;
         }
         switch(currentAiRole.gender){
-            case "boy":
-                msg += "男生";
+            case "male":
+                msg += "boy";
                 break;
-            case "girl":
-                msg += "女生";
+            case "female":
+                msg += "girl";
                 break;
         }
-        chat(msg);
 
-        document.getElementById("settingModal").close();
+        // 清空聊天清單
+        messages.splice(0, messages.length);
+
+        chat(msg);
+        // 關閉設定 modal
+        closeSettingModal();
     }
         
 
@@ -181,12 +185,12 @@
 
 <div class="join join-horizontal absolute bottom-5 left-0 z-55 w-10/10 justify-center bg-gray-200">
     <input type="text" placeholder="想說點什麼呢?" class="input input-info join-item w-7/10" v-model="userMessage" @keyup.enter="send" />
-    <button class="btn join-item bg-gray-700 btn-circle" @click="send">
-        <svg class="size-5 text-white rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+    <button class="btn join-item bg-gray-300 btn-circle hover:bg-blue-300" @click="send">
+        <svg class="size-5 text-gray-700 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M12 2a1 1 0 0 1 .932.638l7 18a1 1 0 0 1-1.326 1.281L13 19.517V13a1 1 0 1 0-2 0v6.517l-5.606 2.402a1 1 0 0 1-1.326-1.281l7-18A1 1 0 0 1 12 2Z" clip-rule="evenodd"/>
         </svg>
     </button>
-    <button class="btn join-item bg-gray-300 btn-circle" @click="openSettingModal">
+    <button class="btn join-item bg-gray-300 btn-circle hover:bg-blue-300" @click="openSettingModal">
         <svg class="size-5 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
         </svg>
@@ -204,44 +208,41 @@
             <div class="text-white">
                 國家:
             </div>
-            <div class="flex flex-row justify-end gap-2">            
+            <div class="flex flex-row justify-start gap-2">            
                 <label class="label text-white ">
-                    <input type="radio" class="radio radio-primary" value="kr" v-model="currentAiRole.nation" />
+                    <input type="radio" class="radio radio-primary" value="Korea" v-model="currentAiRole.nation" />
                     韓國 
                 </label>
                 <label class="label text-white">
-                    <input type="radio" class="radio radio-primary" value="jp" v-model="currentAiRole.nation" />
+                    <input type="radio" class="radio radio-primary" value="Japan" v-model="currentAiRole.nation" />
                     日本
                 </label>
                 <label class="label text-white">
-                    <input type="radio" class="radio radio-primary" value="tw" v-model="currentAiRole.nation" />
+                    <input type="radio" class="radio radio-primary" value="Taiwan" v-model="currentAiRole.nation" />
                     台灣
                 </label>
-                <label class="label text-white">
-                    <input type="radio" class="radio radio-primary" value="us" v-model="currentAiRole.nation" />
-                    美國
-                </label>
             </div>
-            <div class="text-white">
+            <div class="text-white mt-2">
                 性別:
             </div>
             <div class="flex flex-row justify-start gap-2">
                 <label class="label text-white">
-                    <input type="radio" class="radio radio-secondary" value="boy" v-model="currentAiRole.gender" />
+                    <input type="radio" class="radio radio-secondary" value="male" v-model="currentAiRole.gender" />
                     Boy
                 </label>
                 <label class="label text-white">
-                    <input type="radio" class="radio radio-secondary" value="girl" v-model="currentAiRole.gender" />
+                    <input type="radio" class="radio radio-secondary" value="female" v-model="currentAiRole.gender" />
                     Girl
                 </label>
             </div>
         </div>
+        <div class="divider divider-primary"></div>
         <div class="modal-action">
-            <button class="btn btn-ghost w-5/10 text-white" @click="closeSettingModal">
+            <button class="btn btn-ghost w-5/10 text-white hover:text-gray-900" @click="closeSettingModal">
                 關閉設定
             </button>
 
-            <button class="btn btn-ghost w-5/10 text-white" @click="changeAiRole">
+            <button class="btn btn-ghost w-5/10 text-white hover:text-gray-900" @click="changeAiRole">
                 儲存設定
             </button>
         </div>
