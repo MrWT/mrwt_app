7d56e863883470a8d4ab2479c87c160992b4f2ef
advtek02.tn <advtek02.tn@innolux.com>
2025-10-22 16:26:40 +0800

    調整 layout

--- src/components_kuoFund/SettingAnalyze.vue
+++ src/components_kuoFund/SettingAnalyze.vue
@@ -43,15 +43,20 @@
 
 <template>
 
-<div class="relative h-10/10 w-10/10 flex flex-col overflow-y-auto p-2">
-    <button class="btn btn-ghost absolute top-0 right-0 z-10" @click="analyze_survey">
-        <svg class="size-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
-        </svg>
-    </button>
-    <p class="w-10/10 h-10/10" style="white-space:pre-wrap;">
-        {{ analyzeResult }}
-    </p>
+<div class="relative h-10/10 w-10/10 flex flex-col">
+    <div class="w-10/10 h-1/10 flex flex-none justify-end">
+        <button class="btn btn-ghost" @click="analyze_survey">
+            <svg class="size-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
+            </svg>
+        </button>
+    </div>
+    <div class="w-10/10 h-9/10 overflow-y-auto">
+        <p class="w-10/10 h-10/10" style="white-space:pre-wrap;">
+            {{ analyzeResult }}
+        </p>
+        <div class="divider"></div>
+    </div>
 </div>
 
 </template>
