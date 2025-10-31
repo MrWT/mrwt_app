b0b8aa658862aa85a5e17f7f6afd08644b174cce
advtek02.tn <advtek02.tn@innolux.com>
2025-10-31 16:24:05 +0800

    1. 增加"提詞機"功能

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,8 +1,9 @@
 <script setup>
-    import { ref, reactive, onMounted } from 'vue'
+    import { ref, reactive, onMounted, watch } from 'vue'
     import moment from 'moment'
     import { fetchData } from "@/composables/fetchData"
 
+    const emit = defineEmits(['popupMessage']);
     const props = defineProps({
         title: String,
         account: String
@@ -22,6 +23,46 @@
     let userInfo = reactive({});
     let aiRoles = reactive([]);
     let currentAiRole = reactive({});
+    // 提詞機 - 選項
+    let promptOptions = reactive({
+        action: [
+            { value: "summary", text: "重點式整理出", },
+            { value: "list", text: "條列式整理出", },
+        ],
+        nation: [
+            { value: "global", text: "國際", },
+            { value: "taiwan", text: "台灣", },
+            { value: "usa", text: "美國", },
+            { value: "japan", text: "日本", },
+            { value: "korea", text: "韓國", },
+        ],
+        scope: [
+            { value: "house", text: "房地產", },
+            { value: "stock", text: "股市", },
+            { value: "entertainment-ugly", text: "演藝圈(緋聞)", },
+            { value: "entertainment-ceremony", text: "演藝圈(頒獎典禮)", },
+            { value: "entertainment-show", text: "演藝圈(節目)", },
+            { value: "nba", text: "NBA", },
+            { value: "mlb", text: "MLB", },
+        ],
+        time: [
+            { value: "1week", text: "近一週", },
+            { value: "1month", text: "近一個月", },
+            { value: "1day", text: "今天", },
+        ],
+        type: [
+            { value: "news", text: "新聞", },
+            { value: "sport", text: "賽況", },
+        ],
+    });
+    // 提詞機 - 選擇
+    let promptAction = ref("summary");
+    let promptNation = ref("taiwan");
+    let promptScope = ref("stock");
+    let promptTime = ref("1week");
+    let promptType = ref("news");
+    // 提詞機 - 結果
+    let prompt = ref("");
 
     // 初始化 component
     function init(){
@@ -31,6 +72,9 @@
 
         fetchInitData();
         chat("HI");
+
+        // 先組合出預設 prompt
+        combinePrompt();
     }
     // 取得初始資料
     function fetchInitData(){
@@ -163,7 +207,92 @@
         // 關閉設定 modal
         closeSettingModal();
     }
-        
+    // 開啟 prompt modal
+    function openPromptModal(){
+        document.getElementById("promptModal").showModal();
+    }
+    // 組合提詞
+    function combinePrompt(){
+        let c_prompt = "";
+        /*
+        console.log("combinePrompt.promptAction=", promptAction.value);
+        console.log("combinePrompt.promptNation=", promptNation.value);
+        console.log("combinePrompt.promptScope=", promptScope.value);
+        console.log("combinePrompt.promptTime=", promptTime.value);
+        console.log("combinePrompt.promptType=", promptType.value);
+        */
+        if(promptAction.value){
+            promptOptions.action.forEach((actObj, act_i) => {
+                if(promptAction.value === actObj.value){
+                    c_prompt += actObj.text;
+                }
+            });
+        }
+
+        if(promptNation.value){
+            promptOptions.nation.forEach((nationObj, act_i) => {
+                if(promptNation.value === nationObj.value){
+                    c_prompt += nationObj.text;
+                }
+            });
+        }
+
+        if(promptScope.value){
+            promptOptions.scope.forEach((scopeObj, act_i) => {
+                if(promptScope.value === scopeObj.value){
+                    c_prompt += scopeObj.text;
+                }
+            });
+        }
+
+        if(promptTime.value){
+            promptOptions.time.forEach((timeObj, act_i) => {
+                if(promptTime.value === timeObj.value){
+                    c_prompt += timeObj.text;
+                }
+            });
+        }
+
+        if(promptType.value){
+            promptOptions.type.forEach((typeObj, act_i) => {
+                if(promptType.value === typeObj.value){
+                    c_prompt += typeObj.text;
+                }
+            });
+        }
+
+        prompt.value = c_prompt;
+    }
+    // 傳送提詞
+    function sendPrompt(){
+        if(prompt.value){
+            chat_room_uuid.value = "INIT";
+            userMessage.value = prompt.value;
+            send();
+        }else{
+            let error_msg = "你必須提供點內容, 不然提詞機要怎麼幫你轉送~~";
+            emit('popupMessage', false, error_msg); // Emitting the event with data
+        }
+        closePromptModal();
+    }
+    // 關閉 prompt modal
+    function closePromptModal(){
+        document.getElementById("promptModal").close();
+    }
+
+    // 監聽
+    watch(promptAction, (newValue, oldValue) => {
+        combinePrompt();
+    });
+    watch(promptNation, (newValue, oldValue) => {
+        combinePrompt();
+    });
+    watch(promptScope, (newValue, oldValue) => {
+        combinePrompt();
+    });
+    watch(promptType, (newValue, oldValue) => {
+        combinePrompt();
+    });
 
 </script>
 
@@ -205,13 +334,13 @@
             {{ currentAiRole.speaker ?? "AI" }}
         </div>
         <div class="chat-bubble">
-            好的~ 稍等一下喔
+            好喔~ 稍等
             <span class="loading loading-dots loading-xs"></span>
         </div>
     </div>
 </div>
 
-<div class="join join-horizontal absolute bottom-5 left-0 z-55 w-10/10 justify-center md:justify-center bg-gray-200 px-2 gap-2">
+<div class="join join-horizontal absolute bottom-5 left-0 z-10 w-1/1 justify-center md:justify-center bg-gray-200 px-2 gap-2">
     <input type="text" placeholder="想說點什麼呢?" class="input input-info join-item w-6/10" v-model="userMessage" @keyup.enter="send" />
     <button class="btn join-item bg-gray-300 btn-circle hover:bg-blue-300" title="傳送訊息" @click="send">
         <svg class="size-4 text-gray-700 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -223,11 +352,16 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
         </svg>
     </button>
+    <button class="btn join-item bg-gray-300 btn-circle hover:bg-blue-300" title="聊天提詞機" @click="openPromptModal">
+        <svg class="size-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 9h5m3 0h2M7 12h2m3 0h5M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.616a1 1 0 0 0-.67.257l-2.88 2.592A.5.5 0 0 1 8 18.477V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/>
+        </svg>
+    </button>
 </div>
 
 <!-- setting modal -->
 <dialog id="settingModal" class="modal">
-    <div class="modal-box h-8/10 w-8/10 flex flex-col bg-neutral-700">
+    <div class="modal-box h-5/10 w-8/10 flex flex-col bg-neutral-700">
         <div class="flex flex-col justify-center">
             <span class="text-xl text-white text-center">AI 角色設定</span>
             <div class="divider divider-primary"></div>
@@ -267,12 +401,70 @@
         </div>
         <div class="divider divider-primary"></div>
         <div class="modal-action">
-            <button class="btn btn-ghost w-5/10 text-white hover:text-gray-900" @click="closeSettingModal">
+            <button class="btn btn-ghost w-5/10 bg-gray-200 text-gray-900 hover:bg-yellow-100" @click="closeSettingModal">
                 關閉設定
             </button>
 
-            <button class="btn btn-ghost w-5/10 text-white hover:text-gray-900" @click="changeAiRole">
+            <button class="btn btn-ghost w-5/10 bg-gray-200 text-gray-900 hover:bg-yellow-100" @click="changeAiRole">
                 儲存設定
+            </button>
+        </div>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
+
+<!-- prompt modal -->
+<dialog id="promptModal" class="modal modal-end">
+    <div class="modal-box h-10/10 max-w-10/10 flex flex-col bg-neutral-100">
+        <div class="flex flex-col justify-center">
+            <span class="text-lg text-gray-900 text-center">聊天提詞機</span>
+            <div class="divider divider-primary"></div>
+        </div>
+        <div class="h-3/4 md:h-4/5 w-1/1 flex flex-col overflow-y-auto gap-2 border rounded-2xl">
+            <div class="h-1/3 w-1/1 flex flex-row rounded-lg bg-stone-200 px-2 gap-1 overflow-x-auto">
+                <label v-for="(aObj, a_i) in promptOptions.action" class="label text-gray-900">
+                    <input type="radio" class="radio" :value="aObj.value" v-model="promptAction" />
+                    {{ aObj.text }}
+                </label>
+            </div>
+            <div class="h-1/3 w-1/1 flex flex-row rounded-lg bg-state-200 px-2 gap-1 overflow-x-auto">
+                <label v-for="(nObj, n_i) in promptOptions.nation" class="label text-gray-900">
+                    <input type="radio" class="radio" :value="nObj.value" v-model="promptNation" />
+                    {{ nObj.text }}
+                </label>
+            </div>
+            <div class="h-1/3 w-1/1 flex flex-row rounded-lg bg-stone-200 px-2 gap-1 overflow-x-auto">
+                <label v-for="(sObj, s_i) in promptOptions.scope" class="label text-gray-900">
+                    <input type="radio" class="radio" :value="sObj.value" v-model="promptScope" />
+                    {{ sObj.text }}
+                </label>
+            </div>
+            <div class="h-1/3 w-1/1 flex flex-row rounded-lg bg-state-200 px-2 gap-1 overflow-x-auto">
+                <label v-for="(tObj, t_i) in promptOptions.time" class="label text-gray-900">
+                    <input type="radio" class="radio" :value="tObj.value" v-model="promptTime" />
+                    {{ tObj.text }}
+                </label>
+            </div>
+            <div class="h-1/3 w-1/1 flex flex-row rounded-lg bg-stone-200 px-2 gap-1 overflow-x-auto">
+                <label v-for="(tObj, t_i) in promptOptions.type" class="label text-gray-900">
+                    <input type="radio" class="radio" :value="tObj.value" v-model="promptType" />
+                    {{ tObj.text }}
+                </label>
+            </div>
+        </div>
+
+        <textarea class="textarea h-1/4 md:h-1/5 w-1/1 mt-1 bg-gray-900 text-gray-100" placeholder="想說點什麼嗎??" v-model="prompt"></textarea>
+
+        <div class="divider divider-primary"></div>
+        <div class="modal-action">
+            <button class="btn btn-ghost w-5/10 bg-gray-200 text-gray-900 hover:bg-yellow-100" @click.stop="closePromptModal">
+                關閉
+            </button>
+
+            <button class="btn btn-ghost w-5/10 bg-gray-200 text-gray-900 hover:bg-yellow-100" @click.stop="sendPrompt">
+                傳送
             </button>
         </div>
     </div>
