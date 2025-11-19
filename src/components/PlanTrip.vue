8675fd10c130b69ea4b80ed5d4b20f23b600c61f
advtek02.tn <advtek02.tn@innolux.com>
2025-11-19 14:50:13 +0800

    調整"預覽行程" icon

--- src/components/PlanTrip.vue
+++ src/components/PlanTrip.vue
@@ -610,7 +610,8 @@
         <span class="bg-gray-500 text-gray-100 p-1 rounded-full">預覽行程</span>
         <button class="btn btn-circle bg-stone-500/70 hover:bg-blue-300" @click="openSumupModal">
             <svg class="size-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"/>
+                <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
+                <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
             </svg>
         </button>
     </div>
