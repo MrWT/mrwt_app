10605a7b528b5f1b4c25a6522e0fe19428221619
advtek02.tn <advtek02.tn@innolux.com>
2025-12-29 16:00:38 +0800

    調整 tooltip

--- src/App.vue
+++ src/App.vue
@@ -347,15 +347,21 @@
         </div>
         <div class="navbar-end gap-2">
             <!-- readme -->
-            <a class="cursor-pointer tooltip tooltip-bottom" :data-tip="userInfo.languages.readme" @click="gotoPage('readme')">
+            <a class="cursor-pointer tooltip tooltip-bottom" data-tip="系統資訊" @click="gotoPage('readme')">
                 <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
             </a>
             <!-- userInfo -->
-            <a v-if="signinStatus === true" class="cursor-pointer tooltip tooltip-bottom" :data-tip="userInfo.languages.user_setting" @click="openUserInfoModal">
+            <a v-if="signinStatus === true" class="cursor-pointer tooltip tooltip-bottom" data-tip="使用者資訊" @click="openUserInfoModal">
                 <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                     <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd"/>
+                </svg>
+            </a>
+            <!-- signin -->
+            <a v-if="signinStatus === false" class="cursor-pointer tooltip tooltip-bottom" data-tip="登入" @click="gotoPage('login')">
+                <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
                 </svg>
             </a>
         </div>
@@ -364,7 +370,7 @@
     <!-- 功能 component -->
     <div class="p-4 h-11/12">
         <!-- Login -->
-        <Login v-if="appSetting.contentComponent === 'login'" @signin="signin" />
+        <Login v-if="appSetting.contentComponent === 'login'" @signin="signin" @popup-message="popupMessage" />
 
         <!-- component -->
         <Gallery v-if="appSetting.contentComponent === 'gallery'" :title="appSetting.title" :account="userInfo.account" :cname="userInfo.cname" :user_role="userInfo.role" :focus_news_topic="userInfo.focus_news_topic" @popup-message="popupMessage" />
