5630fbac4018f7ab6039e2a23011a92cabc0ffe8
advtek02.tn <advtek02.tn@innolux.com>
2025-12-29 15:06:15 +0800

    將 "signin modal" 獨立成一個 Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
     import { ref, reactive, onMounted } from 'vue'
-    import { gsap } from "gsap"
     import { fetchData } from "./composables/fetchData"
-    import { getRandomNumber } from "./composables/random"
+
+    import Login from './components/Login.vue'
 
     import Finance from './components/Finance.vue'
     import Gallery from './components/Gallery.vue'
@@ -30,25 +30,17 @@
         console.log("App mounted.");
         init();
 
-        // 沒有使用者資訊時, 跳出 signinModal
+        // 沒有使用者資訊時, 就導引至 login.vue
         if(!userInfo.account){
-            document.getElementById("signinModal").showModal();
-            setTimeout(() => {
-                if(!isAutoSignin.value){
-                    document.getElementById("signinName").focus();
-                    genSigninAnimation_gsap();
-                }
-            }, 400);
-
+            appSetting.contentComponent = "login";
+            signinStatus.value = false;
         }
     });
 
-    // 系統資訊
-    let appState = ref("");
     // 一般 - 功能設定
     let appSetting = reactive({
         state: "",
-        contentComponent: "gallery",
+        contentComponent: "login",
         title: "",
         googleMapApiKey: "",
         reference: [],
@@ -71,10 +63,9 @@
             term:"",
         },
     });
+    // 登入狀態
+    let signinStatus = ref(false);
     // 使用者資訊
-    let isAutoSignin = ref(false);
-    let signinError = ref(false);
-    const tempAccount = ref("");
     const userInfo = reactive({
         account: null,
         name: null,
@@ -93,14 +84,15 @@
 
     // 初始化 app
     function init(){
+        resetAppSetting();
+        resetUserInfo();
+
         // 自動登入控制
         {
             const urlParams = new URLSearchParams(window.location.search);
             const user = urlParams.get('user'); // 取得 user
 
             if(user && user.trim()){
-                isAutoSignin.value = true;
-
                 // get_all_account
                 let fetchAllAccount = fetchData({
                     api: "get_all_account",
@@ -111,18 +103,19 @@
 
                     values[0].forEach((account, account_i) => {
                         if(account.toUpperCase() === user.trim().toUpperCase()){
-                            tempAccount.value = account.toUpperCase();
-                            signin();
+                            signin(account.toUpperCase());
                         }
                     });
                 });
+            }else{
+                gotoPage('login');
             }
         }
     }
     // 重設系統設定
     function resetAppSetting(){
-        appSetting.contentComponent = "gallery";
-        appSetting.title = "V.Demo";
+        appSetting.contentComponent = "login";
+        appSetting.title = "K-Assistant";
     }
     // 置換 component
     function gotoPage(page) {
@@ -134,13 +127,10 @@
             appSetting.contentComponent = page;
         }, 200);
     }
-    // keyin account
-    function keyinAccount(event){
-        tempAccount.value = event.target.value.toUpperCase();
-    }
     // 重設使用者資訊
     function resetUserInfo(){
-        tempAccount.value = "";
+        signinStatus.value = false;
+
         userInfo.account = null;
         userInfo.name = null;
         userInfo.shortName = null;
@@ -150,14 +140,10 @@
         userInfo.languages = {};
     }
     // 登入
-    function signin(){
-        //console.log("signin.click");
-        //console.log("tempAccount.value=", tempAccount.value);
+    function signin(account){
+        console.log("app.signin.account=[" + account + "]");
 
-        if(tempAccount.value){
-            // close signinModal
-            document.getElementById("signinModal").close();
-
+        if(account){
             let fetchAppSetting = fetchData({
                 api: "get_app_setting",
             });
@@ -165,14 +151,14 @@
             let fetchUserData = fetchData({
                 api: "login",
                 data: {
-                    account: tempAccount.value.trim()
+                    account: account
                 }
             });
 
             let fetchUserLanguage = fetchData({
                 api: "get_language",
                 data: {
-                    account: tempAccount.value.trim()
+                    account: account
                 }
             });
 
@@ -180,11 +166,11 @@
                 console.log("signin.values=", values);
 
                 if(!values || !values[1]){
-                    signinError.value = true;
+                    signinStatus.value = false;
                     signout();
                     return;
                 }
-                signinError.value = false;
+                signinStatus.value = true;
 
                 let appSettingObj = values[0];
                 console.log("appSettingObj=", appSettingObj);
@@ -271,15 +257,9 @@
                     });
                 }
 
-                // close signinModal
-                document.getElementById("signinModal").close();
-
                 if(userInfoObj["account"].toUpperCase() === "KUOFAMILY" || userInfoObj["role"] === "admin_kf"){
                     // 郭家基金成員 - 預設開啟基金明細 component
                     gotoPage("finance_kf");
-                } else if(userInfoObj["account"].toUpperCase().startsWith("SURVEY")){
-                    // 郭家基金-問卷帳號 - 預設開啟問卷 component
-                    gotoPage("survey");
                 }else{
                     // 預設開啟 gallery component
                     gotoPage('gallery');
@@ -294,10 +274,6 @@
 
         resetUserInfo();
         resetAppSetting();
-        document.getElementById("signinModal").showModal();
-        setTimeout(() => {
-            document.getElementById("signinName").focus();
-        }, 400);
     }
     // 前往作者簡介
     function gotoIntroduceAuthor(data){
@@ -307,44 +283,6 @@
     // 使用者資訊
     function openUserInfoModal(){
         document.getElementById("userInfoModal").showModal();
-    }
-    // 產生登入 modal 動畫
-    function genSigninAnimation_gsap(){
-        let signinModal = document.getElementById("signinModal");
-        console.log("signinModal.clientHeight=", signinModal.clientHeight);
-        console.log("signinModal.clientWidth=", signinModal.clientWidth);
-        
-        // 碎花片 - 候選顏色
-        let boxColors = ["green", "blue", "purple", "gold", "peru", "blanchedalmond", "blueviolet", "goldenrod"];
-        // 碎花片 - 數量上限
-        let boxCount = 500;
-
-        let boxContainer = document.getElementById("boxContainer");
-        for(let box_i = 1; box_i <= boxCount; box_i++){
-            let box_div = document.createElement("div");
-            box_div.setAttribute("id", "box_" + box_i);
-            let boxColorIndex = getRandomNumber(0, boxColors.length - 1);
-            box_div.style.backgroundColor = boxColors[ boxColorIndex ];
-            box_div.style.width = "30px";
-            box_div.style.height = "30px";
-
-            boxContainer.append(box_div);
-        }
-
-        // 控制 gsap animation
-        let tlList = [];
-        for(let box_i = 1; box_i <= boxCount; box_i++){
-            let uniqueId = "" + box_i;
-
-            let tl = gsap.timeline({ yoyo: true, repeat: -1 });
-            tl.to("#" + "box_" + uniqueId, {
-                id: "tween_" + uniqueId,
-                duration: getRandomNumber(10, 20)/10,
-                opacity: 0,
-            });
-            tlList.push(tl);
-        }
-        
     }
     // popup message
     function popupMessage(status, message){
@@ -380,13 +318,15 @@
 
 <template>
     <!-- 系統 loading mask -->
-    <div id="loading" class="w-full h-full flex justify-center items-center bg-gray-700/70 absolute top-0 left-0 z-[9999]" style="display:none;">
-        <span class="loading loading-bars loading-xl"></span>
+    <div id="loading" class="w-1/1 h-1/1 content-center bg-gray-700/70 fixed top-0 left-0 z-[9999] hidden">
+        <div class="w-1/1 text-center">
+            <span class="loading loading-bars loading-xl"></span>
+        </div>
     </div>
 
     <div class="navbar bg-base-100 shadow-sm z-[51] sticky top-0">
         <div class="navbar-start">
-            <div class="dropdown">
+            <div v-if="signinStatus === true" class="dropdown">
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                 </div>
@@ -413,7 +353,7 @@
                 </svg>
             </a>
             <!-- userInfo -->
-            <a class="cursor-pointer tooltip tooltip-bottom" :data-tip="userInfo.languages.user_setting" @click="openUserInfoModal">
+            <a v-if="signinStatus === true" class="cursor-pointer tooltip tooltip-bottom" :data-tip="userInfo.languages.user_setting" @click="openUserInfoModal">
                 <svg class="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                     <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd"/>
                 </svg>
@@ -423,6 +363,10 @@
 
     <!-- 功能 component -->
     <div class="p-4 h-11/12">
+        <!-- Login -->
+        <Login v-if="appSetting.contentComponent === 'login'" @signin="signin" />
+
+        <!-- component -->
         <Gallery v-if="appSetting.contentComponent === 'gallery'" :title="appSetting.title" :account="userInfo.account" :cname="userInfo.cname" :user_role="userInfo.role" :focus_news_topic="userInfo.focus_news_topic" @popup-message="popupMessage" />
         <Readme v-else-if="appSetting.contentComponent === 'readme'" :title="appSetting.title" :reference="appSetting.reference"  @introduce-author="gotoIntroduceAuthor" />
         <Footmark v-else-if="appSetting.contentComponent === 'footmark'" :title="appSetting.title" :account="userInfo.account" :googleMapApiKey="appSetting.googleMapApiKey" @popup-message="popupMessage" />
@@ -435,6 +379,7 @@
         <Recollection v-else-if="appSetting.contentComponent === 'recollection'" :title="appSetting.title" :account="userInfo.account" :user_role="userInfo.role" @popup-message="popupMessage" />
         <Achievement v-else-if="appSetting.contentComponent === 'achievement'" :title="appSetting.title" :account="userInfo.account" :user_role="userInfo.role" />
 
+        <!-- 郭家基金 - component -->
         <QA v-else-if="appSetting.contentComponent === 'qa'" :title="appSetting.title" :account="userInfo.account" :user_role="userInfo.role" @popup-message="popupMessage" />
         <Recall v-else-if="appSetting.contentComponent === 'recall'" :title="appSetting.title" :account="userInfo.account" :user_role="userInfo.role" @popup-message="popupMessage" />
         <Survey v-else-if="appSetting.contentComponent === 'survey'" :title="appSetting.title" :account="userInfo.account" />
@@ -442,31 +387,6 @@
         <Rule_KF v-else-if="appSetting.contentComponent === 'rule_kf'" :title="appSetting.title" :account="userInfo.account" :funSetting="kf_funSetting" />
         <Activity_KF v-else-if="appSetting.contentComponent === 'activity_kf'" :title="appSetting.title" :account="userInfo.account" :user_role="userInfo.role" :annouce_period_start="kf_funSetting.activity_annouce_period.start" :annouce_period_end="kf_funSetting.activity_annouce_period.end" />
     </div>
-
-    <!-- signin modal -->
-    <dialog id="signinModal" class="modal">
-        <div class="modal-box h-4/5 max-w-1/1 flex flex-col place-content-center bg-transparent overflow-hidden">
-
-            <div class="h-auto w-10/10 flex flex-col place-content-center rounded-2xl bg-white p-3 z-51">
-                <h3 class="text-lg font-bold text-center">Hello!</h3>
-                <h4 v-if="signinError" class="text-red-900 text-center mb-5">Error! 登入失敗~ 請檢查登入帳號!</h4>
-                <div v-if="!isAutoSignin" class="flex justify-center items-center join">
-                    <input id="signinName" type="text" placeholder="What's your name?" class="input input-ghost join-item" :value="tempAccount" @change="keyinAccount" @keyup.enter="signin" autofocus />
-                    <button class="btn btn-primary join-item ml-1" @click="signin" >
-                        <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
-                        </svg>
-                    </button>
-                </div>
-                <div v-if="isAutoSignin" class="flex justify-center items-center">
-                    歡迎光臨, 請稍等 <span class="loading loading-dots loading-xs"></span>
-                </div>
-            </div>
-            
-            <div id="boxContainer" class="fixed top-0 left-0 w-10/10 h-10/10 flex flex-wrap justify-center items-center">
-            </div>
-        </div>
-    </dialog>
 
     <!-- userInfo modal -->
     <dialog id="userInfoModal" class="modal">
---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import { ref, reactive, onMounted } from 'vue'
+    
+    const emit = defineEmits(['signin']);
+    const props = defineProps({
+        title: String,
+    })
+
+    onMounted(() => {
+        console.log("Login mounted.");
+        init();
+    });
+
+    let account = ref("");
+
+    // 初始化 component
+    function init(){
+        console.log("Login.props.title=", props.title);
+    }
+    // 登入
+    function signin(){
+        emit('signin', account.value.toUpperCase().trim()); // Emitting the event with data
+    }
+
+</script>
+
+<template>
+
+    <div class="w-1/1 h-1/1 flex flex-col gap-2 place-content-center">
+        <div class="w-1/1 h-1/3 border border-2 border-blue-300 bg-blue-900 text-white rounded-xl p-2 text-3xl place-content-center">
+            歡迎光臨~~!<br />
+            K-Assistant 是您的個人資料管理助理 <br />
+        </div>
+
+        <div class="w-1/1 flex flex-row items-center gap-2">
+            <span class="flex-none text-xl">帳號:</span>
+            <input type="text" class="flex-1 border rounded-xl p-2" v-model="account" />
+            <a class="flex-none border border-2 border-blue-700 bg-blue-500 rounded-xl cursor-pointer text-center text-gray-100 p-2 hover:bg-gray-200 hover:text-gray-900" @click="signin">
+                <span class="text-xl">登入</span>
+            </a>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
