a279c07924ba252f32059c6028a38abe77d9c083
advtek02.tn <advtek02.tn@innolux.com>
2025-12-31 08:38:11 +0800

    調整 login layout

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,19 +30,16 @@
 
 <template>
 
-    <div class="w-1/1 h-1/1 flex flex-col gap-2 place-content-center">
-        <div class="w-1/1 h-1/3 border border-2 border-blue-300 bg-blue-900 text-white rounded-xl p-2 flex flex-col justify-center items-center">
-            <span class="text-5xl underline px-10">K-Assistant</span>
-            <span class="text-2xl">您的個人助理</span>
-        </div>
+    <div class="w-1/1 h-1/1 border border-2 border-blue-300 bg-blue-900 text-white rounded-xl p-2 flex flex-col justify-center items-center">
+        <span class="text-5xl underline px-10">K-Assistant</span>
+        <span class="text-2xl">您的個人助理</span>
+    </div>
 
-        <div class="w-1/1 flex flex-row items-center gap-2">
-            <fieldset class="fieldset flex-1">
-                <legend class="fieldset-legend">What is your name?</legend>
-                <input type="text" class="input w-1/1" placeholder="Type here" v-model="account" />
-            </fieldset>
-            <a class="flex-none border border-2 border-blue-700 bg-blue-900 rounded-xl cursor-pointer text-center text-gray-100 p-2 hover:bg-gray-200 hover:text-gray-900" @click="signin">
-                <svg class="size-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <div class="w-1/1 fixed left-0 bottom-10 flex justify-center">
+        <div class="w-2/3 flex flex-row justify-center items-center join ">
+            <input type="text" class="input join-item" placeholder="請提供帳號" v-model="account" />
+            <a class="join-item border border-2 border-blue-700 bg-sky-400 cursor-pointer text-center text-gray-100 p-2 hover:bg-gray-200 hover:text-gray-900" @click="signin">
+                <svg class="size-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
                 </svg>
             </a>
